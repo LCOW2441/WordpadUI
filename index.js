7af184819bf94fdf816ac33afaa937197cefbd73
Palak <palakv213@gmail.com>
2023-03-16 12:52:43 +0530

    cors

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const app = express();
 
 
 app.use((req, res, next)=>{  
-    res.setHeader("Access-Control-Allow-Origin", "*");  
-    res.setHeader(  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header(  
         "Access-Control-Allow-Headers",  
         "Origin, X-Requested-With, Content-Type, Accept");  
-    res.setHeader("Access-Control-Allow-Methods",  
+    res.header("Access-Control-Allow-Methods",  
     "GET, POST, PATCH, DELETE, OPTIONS");  
     next();  
 });  
